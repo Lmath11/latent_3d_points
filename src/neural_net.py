2b45a0b5bb2e8c85aa04fe7b0039181d91edc33b
Lmath11 <67793708+Lmath11@users.noreply.github.com>
2023-10-22 15:39:37 -0300

    Update neural_net.py

--- src/neural_net.py
+++ src/neural_net.py
@@ -14,12 +14,12 @@ class Neural_Net(object):
 
     def __init__(self, name, graph):
         if graph is None:
-            graph = tf.get_default_graph()
+            graph = tf.compat.v1.get_default_graph()
 
         self.graph = graph
         self.name = name
 
-        with tf.variable_scope(name):
+        with tf.compat.v1.variable_scope(name):
             with tf.device('/cpu:0'):
                 self.epoch = tf.get_variable('epoch', [], initializer=tf.constant_initializer(0), trainable=False)
             self.increment_epoch = self.epoch.assign_add(tf.constant(1.0))
