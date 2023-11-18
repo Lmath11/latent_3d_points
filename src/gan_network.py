bcfc777ea088fac98a9ff56cdac2e15ff411f707
Lmath11 <67793708+Lmath11@users.noreply.github.com>
2023-11-18 09:12:13 -0300

    Update gan_network.py

--- src/gan_network.py
+++ src/gan_network.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from latent_3d_points.layers.gcn import TreeGCN
+from latent_3d_points.layers.gcn import rGAN
 
 from math import ceil
 
