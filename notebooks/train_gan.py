2fc9a86281342c99b416c2597a759b27938d99f5
Lmath11 <67793708+Lmath11@users.noreply.github.com>
2023-12-12 22:50:06 -0300

    Update train_gan.py

--- notebooks/train_gan.py
+++ notebooks/train_gan.py
@@ -164,7 +164,7 @@ class GAN():
                       "[ G_Loss ] ", "{: 7.6f}".format(g_loss), 
                       "[ Tempo/It ] ", "{:4.2f}s".format(time.time()-start_time))
 
-                if _iter % 600 == 0 and _iter !=0:
+                if _iter % 10 == 0 and _iter !=0:
 
             
                     generated_point = self.G.getPointcloud()
