6b830fc21fee1752f2f9039d9e1987e3f22d8c14
Lmath11 <67793708+Lmath11@users.noreply.github.com>
2023-12-31 18:28:48 -0300

    Update train_gan.py

--- notebooks/train_gan.py
+++ notebooks/train_gan.py
@@ -19,22 +19,22 @@ import plotly.graph_objects as go
 import torch.nn.functional as F
 
 CATEGORIES = {
-    #'Airplane': 0,
+    'Airplane': 0,
     'Bag': 1,
-    #'Cap': 2,
-   #'Car': 3,
-    #'Chair': 4,
-    #'Earphone': 5,
-   ## 'Guitar': 6,
-   # 'Knife': 7,
-   # 'Lamp': 8,
-   # 'Laptop': 9,
-   ## 'Motorbike': 10,
-    #'Mug': 11,
-   # 'Pistol': 12,
+    'Cap': 2,
+    'Car': 3,
+    'Chair': 4,
+    'Earphone': 5,
+    'Guitar': 6,
+    'Knife': 7,
+    'Lamp': 8,
+    'Laptop': 9,
+    'Motorbike': 10,
+    'Mug': 11,
+    'Pistol': 12,
     'Rocket': 13,
-    #'Skateboard': 14,
-   # 'Table': 15
+    'Skateboard': 14,
+    'Table': 15
     }
 
 class_choice = ['Bag','Rocket']
