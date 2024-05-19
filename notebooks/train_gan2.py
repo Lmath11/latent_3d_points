e893ea3dac6a7b111bdd545025bef7b748c88017
Lmath11 <67793708+Lmath11@users.noreply.github.com>
2024-05-19 19:25:50 -0300

    Update train_gan2.py

--- notebooks/train_gan2.py
+++ notebooks/train_gan2.py
@@ -163,7 +163,7 @@ class GAN():
                       "[ G_Loss ] ", "{: 7.6f}".format(g_loss), 
                       "[ Tempo/It ] ", "{:4.2f}s".format(time.time()-start_time))
 
-                if _iter % 500000 == 0 and _iter !=0:
+                if _iter % 25 == 0 and _iter !=0:
 
             
                     generated_point = self.G.getPointcloud()
@@ -209,7 +209,7 @@ class GAN():
                     fig.add_trace(go.Scatter(x=new_x, y=loss_D,
                                         mode='lines',
                                         name='Loss D'))
-                    fig.show()
+                    #fig.show()
                     
                     generated_point_cpu = generated_point.to('cpu').detach().numpy()
                     x = generated_point_cpu[:, 2]
