e2e2dc86d55ea67bddde245052c91ee0cfb266b4
Lmath11 <67793708+Lmath11@users.noreply.github.com>
2024-07-21 20:50:29 -0300

    Update arguments.py

--- arguments.py
+++ arguments.py
@@ -6,17 +6,17 @@ class Arguments:
         self._parser = argparse.ArgumentParser(description='Arguments for GAN.')
 
         # Dataset argument
-        self._parser.add_argument('--dataset_path', type=str, default='/content/shapenetcore_partanno_segmentation_benchmark_v0', help='Dataset file path.')
+        self._parser.add_argument('--dataset_path', type=str, default='/content/shapenetcore_partanno_segmentation_benchmark_v0_balanceado', help='Dataset file path.')
         self._parser.add_argument('--class_choice', type=str, help='Select one class to generate. [plane, chair, ...] (default:all_class)')
         self._parser.add_argument('--batch_size', type=int, default=5, help='Integer value for batch size.')
-        self._parser.add_argument('--point_num', type=int, default=5096, help='Integer value for number of points.')
+        self._parser.add_argument('--point_num', type=int, default=2048, help='Integer value for number of points.')
         
         # Training argument
         self._parser.add_argument('--gpu', type=int, default=0, help='GPU number to use.')
         self._parser.add_argument('--epochs', type=int, default=4001, help='Integer value for epochs.')
         self._parser.add_argument('--lr', type=float, default=1e-4, help='Float value for learning rate.')
-        self._parser.add_argument('--ckpt_path', type=str, default='/content/drive/MyDrive/ResultadosLothar/Treegan/All/', help='Checkpoint path.')
-        self._parser.add_argument('--ckpt_save', type=str, default='TreeGAN_All_5K_ckpt_', help='Checkpoint name to save.')
+        self._parser.add_argument('--ckpt_path', type=str, default='/content/drive/MyDrive/ResultadosLothar/Treegan/Balanceado/', help='Checkpoint path.')
+        self._parser.add_argument('--ckpt_save', type=str, default='TreeGAN_Balanceado_ckpt_', help='Checkpoint name to save.')
         self._parser.add_argument('--ckpt_load', type=str, help='Checkpoint name to load.')
         self._parser.add_argument('--visdom_port', type=int, default=8097, help='Visdom port number. (default:8097)')
         self._parser.add_argument('--visdom_color', type=int, default=4, help='Number of colors for visdom pointcloud visualization. (default:4)')
