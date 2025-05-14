03941a330766b724bf5ad60c404a1db113bcfc38
Lmath11 <67793708+Lmath11@users.noreply.github.com>
2025-05-13 22:08:17 -0300

    Update dataset_benchmark.py

--- src/dataset_benchmark.py
+++ src/dataset_benchmark.py
@@ -5,7 +5,7 @@ import os.path
 import torch
 import numpy as np
 
-class_choice = ["Airplane","Chair"]
+#class_choice = ["Airplane","Chair"]
 
 class BenchmarkDataset(data.Dataset):
     def __init__(self, root, npoints=2500, uniform=False, classification=True):
